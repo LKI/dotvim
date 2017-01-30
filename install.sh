e76b0c1d980342be6fd0e53334b78c7b0a75c293
Lirian Su <liriansu@gmail.com>
2017-01-30 15:54:11 +0800

    use "set -e"

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-git clone --recurse-submodules git://github.com/LKI/dotvim.git ~/.vim && ln -sf ~/.vim/vimrc ~/.vimrc && vim +PluginInstall +qall
+set -e
+git clone --recurse-submodules git://github.com/LKI/dotvim.git ~/.vim
+ln -sf ~/.vim/vimrc ~/.vimrc
+vim +PluginInstall +qall
